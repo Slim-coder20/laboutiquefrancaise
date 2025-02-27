a607890d4cd3cbc9bcf289046fb045496f57fa42
slim <slimabida21@gmail.com>
2025-02-27 19:30:56 +0100

    create contraints in the RegisterUserType test OK

--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -11,6 +12,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterUserType extends AbstractType
 {
@@ -18,12 +20,30 @@ class RegisterUserType extends AbstractType
     {
         $builder
             ->add('firstname',TextType::class,[
+
+            'constraints' => [
+                    
+                new Length([
+                    'min' => 6,
+                    'max' => 20
+                ])
+            
+            ],
+
                 'label' => 'Prénom',
                 'attr' => [
                     'placeholder' => 'Entrez votre prénom'
                 ]
             ])
             ->add('lastname',TextType::class, [
+                'constraints' => [
+                    
+                new Length([
+                    'min' => 6,
+                    'max' => 20
+                ])
+            
+            ],
                 'label' => 'Nom',
                 'attr' =>[
                     'placeholder' => 'Entrez votre nom'
@@ -40,6 +60,15 @@ class RegisterUserType extends AbstractType
             ->add('plainPassword', RepeatedType::class,[
 
                 'type' => PasswordType::class, 
+                'constraints' => [
+                    
+                new Length([
+                    'min' => 6,
+                    'max' => 30
+                ])
+            
+            ],
+                
                 'first_options'  => [
                 
                 'label' => 'Votre mot de passe ', 
@@ -79,6 +108,14 @@ class RegisterUserType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
+            'constraints' => [
+                new UniqueEntity([
+                    'entityClass' => User::class,
+                    'fields' => 'email'
+
+                ])
+
+            ],
             'data_class' => User::class,
         ]);
     }
