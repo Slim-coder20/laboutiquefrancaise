e01a4507b9a1b89993b0a54b7131ca6b6f3a17f5
slim <slimabida21@gmail.com>
2025-03-11 15:36:07 +0100

    mise en place de la wishlist dans le compte utilisateur test OK

--- src/Controller/Account/WishlistController.php
+++ src/Controller/Account/WishlistController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Account;
 
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -12,15 +15,65 @@ final class WishlistController extends AbstractController
     public function index(): Response
     {
         
+    return $this->render('account/wishlist/index.html.twig');
+     }
+
+
+
+  
+    #[Route('/compte/liste-de-souhait/add/{id}', name: 'app_account_wishlist_add')]
+    public function add(ProductRepository $productRepository, EntityManagerInterface $entityManager,$id,Request $request): Response
+    {
+        // 1. récupérer l'objet du produit souhaité 
+        $product = $productRepository->findOneById($id);
+        // 2. Si produit existant . ajouter le produit à la wishlist 
+        if($product){
+            $this->getUser()->addWishlist($product);
+              // 3. Sauvegarder en baese de donnée 
+            $entityManager->flush();
+        }
+
+        $this ->addFlash(
+
+            'success',
+            'Produit correctement ajouté à votre liste de souhait '
+        );
+
+
+        return $this->redirect($request->headers->get('referer'));
         
-        
-        
-        
-        
-        
-        
-        return $this->render('account/wishlist/index.html.twig', [
-            'controller_name' => 'WishlistController',
-        ]);
+      
+       
     }
+    #[Route('/compte/liste-de-souhait/remove/{id}', name: 'app_account_wishlist_remove')]
+    public function remove(ProductRepository $productRepository, EntityManagerInterface $entityManager,$id): Response
+    {
+        // 1. récupérer l'objet du produit à supprimer  
+        $product = $productRepository->findOneById($id);
+        
+        // 2. Si produit existant . supprimer le produit de la liste wishlist 
+        if($product){
+            $this->addFlash('success', 'Produit correctement supprimé de votre liste de souhait.');
+            $this->getUser()->removeWishlist($product);
+            $entityManager->flush();
+        }else {
+
+            $this->addFlash('danger', 'Produit introuvable ');
+
+        }
+        return $this->redirectToRoute('app_account_wishlist');
+        // 3. Sauvegarder en baese de donnée 
+       
+    }
+
+    
+
+
+
+
+
+
+
+
+
 }
